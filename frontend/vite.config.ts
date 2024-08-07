1d93e9350b361f4f39bba8980a82d37e3d67f249
shaima.kassem@ihec.ucar.tn <shaima.kassem@ihec.ucar.tn>
2024-08-07 01:16:35 +0100

    implementation redux

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'js', // Use jsx loader for .js files
+    include: /src\/.*\.js$/, // Apply only to .js files in the src directory
+  },
+});
